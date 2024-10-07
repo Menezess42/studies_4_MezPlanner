b3a5e276bdc9ccdf13cd73db815fdf160257c269
Menezess42 <arielmenezess@hotmail.com>
2024-10-07 15:48:29 -0300

    finished the user authentication studie

--- flask_database_sqlAlchemy/app.py
+++ flask_database_sqlAlchemy/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 # we gona have a db object that is gonna be needed in the models.py file
 db = SQLAlchemy()
@@ -11,14 +12,27 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__, template_folder="templates")
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///./testdb.db"
+    app.secret_key='SOME KEY'
 
     db.init_app(app)
 
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    from models import User
+
+    # defining for the LoginManager what is a Logad user
+    @login_manager.user_loader
+    def logad_user(uid):
+        return User.query.get(uid)
+
     # imports later on
-    ## Avoiding circular imports(infinite recursive)
+    # Avoiding circular imports(infinite recursive)
     from routes import register_routes
 
-    register_routes(app, db)
-    ##
+    bcrypt = Bcrypt(app)
+
+    register_routes(app, db, bcrypt)
+
     migrate = Migrate(app, db)
     return app
--- flask_database_sqlAlchemy/routes.py
+++ flask_database_sqlAlchemy/routes.py
@@ -1,42 +1,88 @@
-# File where we define the routes
-from flask import render_template, request
+# File where we define the routes, redirect
+from flask import render_template, request, redirect, url_for
+from flask_login import login_user, logout_user, current_user, login_required
 
-from models import Person
+from models import User
 
 
-def register_routes(app, db):
+def register_routes(app, db, bcrypt):
+
     @app.route("/", methods=["GET", "POST"])
     def index():
+        return render_template("index.html")
+
+    @app.route("/signup", methods=["GET", "POST"])
+    def signup():
         if request.method == "GET":
-            people = Person.query.all()
-            return render_template("index.html", people=people)
+            return render_template("signup.html")
         elif request.method == "POST":
-            name = request.form.get("name")
-            age = int(request.form.get("age"))
-            job = request.form.get("job")
+            username = request.form.get("username")
+            password = request.form.get("password")
 
-            person = Person(name=name, age=age, job=job)
-            db.session.add(person)
+            hashed_password = bcrypt.generate_password_hash(password)
+            user = User(username=username, password=hashed_password)
+
+            db.session.add(user)
             db.session.commit()
+            return redirect(url_for("index"))
 
-            people = Person.query.all()
-            return render_template("index.html", people=people)
+    @app.route("/login", methods=["GET", "POST"])
+    def login():
+        if request.method == "GET":
+            return render_template("login.html")
+        elif request.method == "POST":
+            username = request.form.get("username")
+            password = request.form.get("password")
+
+            user = User.query.filter(User.username == username).first()
+
+            if bcrypt.check_password_hash(user.password, password):
+                login_user(user)
+                return render_template('index.html')
+            else:
+                return 'Failed'
+                
+
+    @app.route("/logout", methods=["GET", "POST"])
+    def logout():
+        logout_user()
+        return render_template('index.html')
+
+    @app.route('/secret')
+    @login_required
+    def secret():
+        return "Secret section!!!"
 
 
-    @app.route("/delete/<pid>", methods=["DELETE"])
-    def delete(pid):
-        Person.query.filter(Person.pid == pid).delete()
+# def register_routes(app, db):
+#     @app.route("/", methods=["GET", "POST"])
+#     def index():
+#         if request.method == "GET":
+#             people = Person.query.all()
+#             return render_template("index.html", people=people)
+#         elif request.method == "POST":
+#             name = request.form.get("name")
+#             age = int(request.form.get("age"))
+#             job = request.form.get("job")
 
-        db.session.commit()
-        people = Person.query.all()
-        return render_template('index.html', people=people)
+#             person = Person(name=name, age=age, job=job)
+#             db.session.add(person)
+#             db.session.commit()
+
+#             people = Person.query.all()
+#             return render_template("index.html", people=people)
 
 
-    @app.route('/details/<pid>')
-    def details(pid):
-        person = Person.query.filter(Person.pid == pid).first()
-        return render_template('detail.html', person=person)
+#     @app.route("/delete/<pid>", methods=["DELETE"])
+#     def delete(pid):
+#         Person.query.filter(Person.pid == pid).delete()
+
+#         db.session.commit()
+#         people = Person.query.all()
+#         return render_template('index.html', people=people)
 
 
-
-
+#     @app.route('/details/<pid>')
+#     def details(pid):
+#         person = Person.query.filter(Person.pid == pid).first()
+#         return render_template('detail.html', person=person)
